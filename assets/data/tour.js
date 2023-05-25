dfa95caaac66dd9329a2c7e5dfb94b6affd6c224
ardiavi <ardiambis@gmail.com>
2023-05-25 20:35:19 +0700

    Melengkapi Array of Assets

--- assets/data/tour.js
+++ assets/data/tour.js
@@ -4,9 +4,20 @@ const tour = [
         name: 'Tour Jogja 3 Hari 2 Malam',
         location: 'Sleman, Daerah Istimewa Yogyakarta',
         reviwes: 4.5,
-        price: 'Rp. 400,000',
+        price: 'Rp. 4,000,000',
         status: 'Tersedia',
-        description: 'Paket tour ini untuk 5 orang sudah termasuk penginapan, tiket, dan transportasi. Wisatawan akan diajak berkeliling jogja untuk menikmati wisata budaya dan alamnya.'
+        description: 'Paket tour ini untuk 5 orang sudah termasuk penginapan, tiket, dan transportasi. Wisatawan akan diajak berkeliling jogja untuk menikmati wisata budaya dan alamnya.',
+        image: require('../images/slemancity.png')
+    },
+    {
+        id: 2,
+        name: 'Tour Bandung 2 Hari 1 Malam',
+        location: 'Bandung, Jawa Barat',
+        reviwes: 4.7,
+        price: 'Rp. 1,000,000',
+        status: 'Tersedia',
+        description: 'Paket tour ini untuk 4 orang sudah termasuk penginapan, tiket, dan transportasi. Wisatawan akan diajak berkeliling bandung untuk menikmati wisata budaya dan alamnya.',
+        image: require('../images/bandungcity.png')
     }
 ]
 
