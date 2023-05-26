a8bb048f53499dd24493fc69dce57a1f8e92380d
BarakaKautsar <36478251+BarakaKautsar@users.noreply.github.com>
2023-05-26 14:55:26 +0700

    historyyyyyy

---
+++ assets/data/history.js
@@ -0,0 +1,37 @@
+const paymentHistory = [
+    {
+      id: '1',
+      name: 'Grand Hyatt',
+      city: 'Jakarta',
+      type: 'Hotels',
+      date: '2023-03-01',
+      totalCost: 'Rp 2,000,000'
+    },
+    {
+      id: '2',
+      name: 'Bali Full Day Tour',
+      city: 'Bali',
+      type: 'Tour',
+      date: '2023-04-15',
+      totalCost: 'Rp 1,500,000'
+    },
+    {
+      id: '3',
+      name: 'Flight to Yogyakarta',
+      city: 'Yogyakarta',
+      type: 'Ticket',
+      date: '2023-06-12',
+      totalCost: 'Rp 800,000'
+    },
+    {
+      id: '4',
+      name: 'Shangri-La Hotel',
+      city: 'Surabaya',
+      type: 'Hotels',
+      date: '2023-07-20',
+      totalCost: 'Rp 1,800,000'
+    },
+    // more data...
+  ];
+
+export default paymentHistory;
--- screens/History.js
+++ screens/History.js
@@ -1,15 +1,106 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity, Image, FlatList} from 'react-native'
+import React, { useEffect, useState } from 'react'
+import colors from '../assets/colors.js'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import paymentHistory from '../assets/data/history.js';
 
-const History = () => {
+const History = ({navigation}) => {
+  const [search, setSearch] = useState('');
+
+  const renderItem = ({ item }) => (
+    <View style={styles.itemContainer}>
+      <Text style={styles.itemName}>{item.name}</Text>
+      <Text style={styles.itemDetails}>{item.city} - {item.type}</Text>
+      <Text style={styles.itemDate}>Tanggal: {item.date}</Text>
+      <Text style={styles.itemCost}>Harga: {item.totalCost}</Text>
+    </View>
+  );
+
   return (
+    <View style={{backgroundColor:colors.blue}}>
     <SafeAreaView>
-      <Text>History</Text>
+    <View style={styles.header}>
+        <Text style={styles.BigText}>Riwayat Pesanan</Text>
+    </View>
+    <View style={styles.container}>
+        <View style={styles.searchSection}>
+          <MaterialCommunityIcons style = {{padding: 10}} name="magnify" size={24} color={colors.darkgrey} />
+          <TextInput
+            style={styles.input}
+            placeholder="Cari Riwayat Pesanan"
+            value={search}
+            onChangeText={setSearch}
+          />
+        </View>
+        <ScrollView horizontal={true}>
+        <FlatList
+        data={paymentHistory}
+        keyExtractor={item => item.id}
+        renderItem={renderItem}
+        style={{width: 350, marginTop: 20}}
+      />
+        </ScrollView>
+    </View>
     </SafeAreaView>
+    </View>
   )
 }
 
 export default History
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+container: {
+    backgroundColor: "white",
+    height: 800,
+    alignItems: 'left',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    padding: 10,
+  },
+BigText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    width: 350,
+    color: '#fff',
+    padding: 20,
+    alignSelf: 'center',
+  },
+header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+},
+searchSection: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  backgroundColor: colors.lightgrey,
+  width: "100%",
+  borderRadius: 20,
+},
+itemContainer: {
+  marginBottom: 20,
+  borderWidth: 1,
+  borderColor: '#ccc',
+  padding: 10,
+  borderRadius: 5,
+},
+itemName: {
+  fontSize: 18,
+  marginBottom: 5,
+},
+itemDetails: {
+  fontSize: 16,
+  color: '#444',
+  marginBottom: 5,
+},
+itemDate: {
+  fontSize: 16,
+  color: '#444',
+  marginBottom: 5,
+},
+itemCost: {
+  fontSize: 16,
+  color: '#444',
+  marginBottom: 5,
+},
+});
